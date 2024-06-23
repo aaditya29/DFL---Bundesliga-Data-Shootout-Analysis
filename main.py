3b56815bcc0ec65ffed9c1ea7f352b23d84c59d2
Aditya Mishra <mishraaaditya29@gmail.com>
2024-06-23 14:40:20 +0530

    read and save video function added

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from utils import read_video, save_video
+
+
 def main():
-    print("Hello, World!")
+
+    # Reading video
+    video_frames = read_video(
+        '/Users/adityamishra/Documents/Football-Analysis/input-videos/08fd33_4.mp4')
+
+    # Saving video
+    save_video(video_frames, 'output_videos/output_video.avi')
 
 
 if __name__ == "__main__":
