fd9d574b69521e075da7378a202e92e94a0e2794
Aditya Mishra <mishraaaditya29@gmail.com>
2024-06-24 09:24:32 +0530

    initialised trackers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from utils import read_video, save_video
+from trackers import Tracker
 
 
 def main():
@@ -6,6 +7,12 @@ def main():
     # Reading video
     video_frames = read_video(
         '/Users/adityamishra/Documents/Football-Analysis/input-videos/08fd33_4.mp4')
+
+    # Initialising Tracker
+    tracker = Tracker('models/best.pt')
+
+    # Getting object
+    tracks = tracker.get_object_tracks(video_frames)
 
     # Saving video
     save_video(video_frames, 'output_videos/output_video.avi')
