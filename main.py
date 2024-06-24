60bee494934efdf45f6c35dfded12c9750c5bab9
Aditya Mishra <mishraaaditya29@gmail.com>
2024-06-24 20:27:30 +0530

    added draw_annotations

--- main.py
+++ main.py
@@ -16,8 +16,11 @@ def main():
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
 
+    # Calling draw output function for object tracks
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+
     # Saving video
-    save_video(video_frames, 'output_videos/output_video.avi')
+    save_video(output_video_frames, 'output_videos/output_video.avi')
 
 
 if __name__ == "__main__":
