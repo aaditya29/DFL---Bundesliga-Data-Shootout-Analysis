45c8fdfbe1a27bd17696caa347feb85f48c6b324
Aditya Mishra <mishraaaditya29@gmail.com>
2024-06-25 18:35:03 +0530

    added cropped image function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video
 from trackers import Tracker
+import cv2
 
 
 def main():
@@ -15,6 +16,19 @@ def main():
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
+
+    # Saving cropped image of the player for analysis
+    for track_id, player in tracks['players'].items():
+        bbox = player['bbox']
+        frame = video_frames[0]
+
+        # cropping bbox from the frame
+        cropped_image = frame[int(bbox[1]):int(
+            bbox[3]), int(bbox[0]):int(bbox[2])]
+
+        # saving the image
+        cv2.imwrite(f'output_videos/cropped_image.jpg', cropped_image)
+        break
 
     # Calling draw output function for object tracks
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
