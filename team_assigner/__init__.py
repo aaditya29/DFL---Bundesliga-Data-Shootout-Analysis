24b431afab89cd3c00d584d79dc051672789e06b
Aditya Mishra <mishraaaditya29@gmail.com>
2024-06-26 18:25:01 +0530

    added kmeans model

--- team_assigner/__init__.py
+++ team_assigner/__init__.py
@@ -1,5 +1,31 @@
+from sklearn.cluster import KMeans
+
+
 class TeamAssigner:
     def __init__(self):
         pass
 
+    def get_clustering_model(self, image):
+        # Reshaping the image to 2D array
+        image_2d = image.reshape(-1, 3)
+
+        # Preform K-means with 2 clusters
+        kmeans = KMeans(n_clusters=2, init="k-means++", n_init=1)
+        kmeans.fit(image_2d)
+
+        return kmeans
+
+    def get_player_color(self, frame, bbox):
+
+        # cropping image
+        image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+        # getting top half of the image
+        top_half_image = image[0:int(image.shape[0]/2), :]
+
     def assign_team_color(self, frame, player_detections):
+        player_colors = []
+
+        for _, player_detection in player_detections.items():
+            bbox = player_detection["bbox"]
+            player_color = self.get_player_color(frame, bbox)
