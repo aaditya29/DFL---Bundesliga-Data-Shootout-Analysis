f07e9859f4ffd1a0a029ec2f89a3c6026f7fa3bd
Aditya Mishra <mishraaaditya29@gmail.com>
2024-06-26 18:40:33 +0530

    git commit

--- team_assigner/__init__.py
+++ team_assigner/__init__.py
@@ -3,7 +3,8 @@ from sklearn.cluster import KMeans
 
 class TeamAssigner:
     def __init__(self):
-        pass
+        self.team_colors = {}
+        self.player_team_dict = {}
 
     def get_clustering_model(self, image):
         # Reshaping the image to 2D array
@@ -23,9 +24,53 @@ class TeamAssigner:
         # getting top half of the image
         top_half_image = image[0:int(image.shape[0]/2), :]
 
+        # Getting Clustering model
+        kmeans = self.get_clustering_model(top_half_image)
+
+        # Getting the cluster labels forr each pixel
+        labels = kmeans.labels_
+
+        # Reshaping the labels to the image shape
+        clustered_image = labels.reshape(
+            top_half_image.shape[0], top_half_image.shape[1])
+
+        # Getting the player cluster
+        corner_clusters = [clustered_image[0, 0], clustered_image[0, -1],
+                           clustered_image[-1, 0], clustered_image[-1, -1]]
+        non_player_cluster = max(set(corner_clusters),
+                                 key=corner_clusters.count)
+        player_cluster = 1 - non_player_cluster
+
+        player_color = kmeans.cluster_centers_[player_cluster]
+
+        return player_color
+
     def assign_team_color(self, frame, player_detections):
         player_colors = []
 
         for _, player_detection in player_detections.items():
             bbox = player_detection["bbox"]
             player_color = self.get_player_color(frame, bbox)
+            player_colors.append(player_color)
+
+        # Divding the colours int two
+        kmeans = KMeans(n_clusters=2, init="k-means++", n_init=10)
+        kmeans.fit(player_colors)
+
+        self.kmeans = kmeans  # saving into kmeans
+
+        self.team_colors[1] = kmeans.cluster_centers_[0]
+        self.team_colors[2] = kmeans.cluster_centers_[1]
+
+    def get_player_team(self, frame, player_bbox, player_id):
+        if player_id in self.player_team_dict:
+            return self.player_team_dict[player_id]
+
+        player_color = self.get_player_color(frame, player_bbox)
+
+        team_id = self.kmeans.predict(player_color.reshape(1, -1))[0]
+        team_id += 1
+
+        self.player_team_dict[player_id] = team_id
+
+        return team_id
