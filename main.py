534d82eceb7e916c4cd66838109a166167934830
Aditya Mishra <mishraaaditya29@gmail.com>
2024-06-26 19:03:00 +0530

    added team assignment loop

--- main.py
+++ main.py
@@ -24,6 +24,14 @@ def main():
     team_assigner.assign_team_color(video_frames[0],
                                     tracks['players'][0])
 
+    # Looping over each player in each frame and assigning them to colour team
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],
+                                                 track['bbox'], player_id)
+            tracks['players'][frame_num][player_id]['team'] = team
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
     # Calling draw output function for object tracks
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
