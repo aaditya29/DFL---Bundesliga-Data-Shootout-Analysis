1ad67da926009af057619a772306151994bb97e0
Aditya Mishra <mishraaaditya29@gmail.com>
2024-06-30 18:54:07 +0530

    git commit

--- player_ball_assigner/player_ball_assigner.py
+++ player_ball_assigner/player_ball_assigner.py
@@ -10,4 +10,9 @@ class PlayerBallAssigner():
         self.max_player_ball_distance = 70
 
     def assign_ball_to_player(self, players, ball_bbox):
+        # getting center of the ball box as ball position
         ball_position = get_center_of_bbox(ball_bbox)
+
+        # looping over each player
+        for player_id, player in players.items():
+            player_bbox = player['bbox']
