ee133f0f9a8f7680e326b8d8d535d7f4fe698f64
Aditya Mishra <mishraaaditya29@gmail.com>
2024-06-30 17:41:57 +0530

    added assign_ball_to_player function

--- player_ball_assigner/player_ball_assigner.py
+++ player_ball_assigner/player_ball_assigner.py
@@ -8,3 +8,6 @@ class PlayerBallAssigner():
     def __init__(self):
         # setting max player ball distance as 70 pixels
         self.max_player_ball_distance = 70
+
+    def assign_ball_to_player(self, players, ball_bbox):
+        ball_position = get_center_of_bbox(ball_bbox)
