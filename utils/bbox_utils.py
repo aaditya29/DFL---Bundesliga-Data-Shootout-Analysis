410e022d71e93065daee3d07e273a43b3897eae4
Aditya Mishra <mishraaaditya29@gmail.com>
2024-07-01 11:14:11 +0530

    added calculation of feet distance

--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -5,3 +5,21 @@ def get_center_of_bbox(bbox):
 
 def get_bbox_width(bbox):
     return bbox[2]-bbox[0]
+
+
+# measuring distance between two points for nearest feet
+"""
+Here p1[0] and p2[0] are the x-coordinates, p1[1] and p2[1] are the y-coordinates.
+
+The function uses the Euclidean distance formula:
+distance = √[(x2 - x1)^2 + (y2 - y1)^2]
+
+(p1[0]-p2[0])**2: Calculates the squared difference of x-coordinates
+(p1[1]-p2[1])**2: Calculates the squared difference of y-coordinates
+The results are added together AND
+multiplied by 0.5 is used to calculate the square root of the sum.
+"""
+
+
+def measure_distance(p1, p2):
+    return ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)**0.5
