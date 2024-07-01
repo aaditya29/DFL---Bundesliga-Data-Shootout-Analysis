3bbd4fb983234c8fc71b59721e5d37ae844adfc1
Aditya Mishra <mishraaaditya29@gmail.com>
2024-07-01 11:24:26 +0530

    player ball assignment completed

--- player_ball_assigner/player_ball_assigner.py
+++ player_ball_assigner/player_ball_assigner.py
@@ -1,5 +1,5 @@
 import sys
-from utils import get_center_of_bbox
+from utils import get_center_of_bbox, measure_distance
 sys.path.append('../')
 
 
@@ -13,6 +13,24 @@ class PlayerBallAssigner():
         # getting center of the ball box as ball position
         ball_position = get_center_of_bbox(ball_bbox)
 
+        # getting closest player
+        miniumum_distance = 99999
+        assigned_player = -1
+
         # looping over each player
         for player_id, player in players.items():
             player_bbox = player['bbox']
+
+            distance_left = measure_distance(
+                (player_bbox[0], player_bbox[-1]), ball_position)
+            distance_right = measure_distance(
+                (player_bbox[2], player_bbox[-1]), ball_position)
+            # real distance is going to be minimum dist between the two
+            distance = min(distance_left, distance_right)
+
+            if distance < self.max_player_ball_distance:
+                if distance < miniumum_distance:
+                    miniumum_distance = distance
+                    assigned_player = player_id
+
+        return assigned_player
