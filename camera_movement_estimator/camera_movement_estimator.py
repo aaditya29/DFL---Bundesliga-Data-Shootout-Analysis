c9913876fd617f73940ec455edeb296095e3e544
Aditya Mishra <mishraaaditya29@gmail.com>
2024-07-07 09:47:11 +0530

    added camera movement estimator class

--- camera_movement_estimator/camera_movement_estimator.py
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -1,3 +1,15 @@
 import cv2
+import pickle
 import numpy as np
 import matplotlib as plt
+
+
+class CameraMovementEstimator:
+    def __init__(self):
+        pass
+
+    def get_camera_movement(self, frames, read_from_stub=False, stub_path=None):
+
+        # Reading the stub from the file
+        # movement for x and movement for y coordinates and multiplying by frames
+        camera_movement = [[0, 0]]*len(frames)
