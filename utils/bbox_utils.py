111e526c61467d76177001133b4e0012d08551f6
Aditya Mishra <mishraaaditya29@gmail.com>
2024-07-08 17:03:46 +0530

    added measure_xy_distance

--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -23,3 +23,7 @@ multiplied by 0.5 is used to calculate the square root of the sum.
 
 def measure_distance(p1, p2):
     return ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)**0.5
+
+
+def measure_xy_distance(p1, p2):
+    return p1[0]-p2[0], p1[1]-p2[1]
