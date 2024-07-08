ffb121a2fd2ce54387b0157392ee0094d9ec35fa
Aditya Mishra <mishraaaditya29@gmail.com>
2024-07-08 17:40:24 +0530

    added camera movement

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils import read_video, save_video
 from trackers import Tracker
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
+from camera_movement_estimator import CameraMovementEstimator
 import cv2
 import numpy as np
 
@@ -19,6 +20,13 @@ def main():
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
+
+    # Adding camera movement estimator
+    # Initialising by first frame
+    camera_movement_estimator = CameraMovementEstimator(video_frames[0])
+    camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames,
+                                                                              read_from_stub=True,
+                                                                              stub_path='stubs/camera_movement_stub.pkl')
 
     # Interpolating/inserting ball positions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
@@ -53,9 +61,14 @@ def main():
             # last person who has the ball
             team_ball_control.append(team_ball_control[-1])
     team_ball_control = np.array(team_ball_control)
+
     # Calling draw output function for object tracks
     output_video_frames = tracker.draw_annotations(
         video_frames, tracks, team_ball_control)
+
+    # Drawing camera movement
+    output_video_frames = camera_movement_estimator.draw_camera_movement(
+        output_video_frames, camera_movement_per_frame)
 
     # Saving video
     save_video(output_video_frames, 'output_videos/output_video.avi')
