30e8b442bcd1b4e0c026c5e242ef1174a5fe11f9
Aditya Mishra <mishraaaditya29@gmail.com>
2024-07-10 19:05:06 +0530

    git commit

--- view_transformer/view_transformer.py
+++ view_transformer/view_transformer.py
@@ -32,12 +32,20 @@ class ViewTransformer():
             self.pixel_vertices, self.target_vertices)
 
     def add_transformed_position_to_tracks(self, tracks):
+        # Iterating over each object and its tracks in the dictionary
         for object, object_tracks in tracks.items():
+            # Iteraring over each frame's track in the list of object tracks
             for frame_num, track in enumerate(object_tracks):
+                # Iterating over each track ID and its info in the dictionary
                 for track_id, track_info in track.items():
+                    # Extracting ethe 'position_adjusted' value from track_info
                     position = track_info['position_adjusted']
+                    # Converting the position to a NumPy array
                     position = np.array(position)
+                    # Applying a transformation to the position using a method called transform_point.
                     position_transformed = self.transform_point(position)
+                    # If the transformed position is not None, squeeze and convert to a list
                     if position_transformed is not None:
                         position_transformed = position_transformed.squeeze().tolist()
+                    # Updating the tracks dictionary with the transformed position
                     tracks[object][frame_num][track_id]['position_transformed'] = position_transformed
