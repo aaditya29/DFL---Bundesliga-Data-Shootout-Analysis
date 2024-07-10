bbf742dd6fafe94f16b0928d994e157f1db7c4bc
Aditya Mishra <mishraaaditya29@gmail.com>
2024-07-10 19:16:03 +0530

    View Transformed Completed

--- view_transformer/view_transformer.py
+++ view_transformer/view_transformer.py
@@ -31,6 +31,38 @@ class ViewTransformer():
         self.persepctive_transformer = cv2.getPerspectiveTransform(
             self.pixel_vertices, self.target_vertices)
 
+    def transform_point(self, point):
+        # Converting point to the integer co-ordinates
+        """
+        point[0]: The x-coordinate of the point.
+        point[1]: The y-coordinate of the point.
+        int(): Converts the coordinates to integer values.
+        """
+        p = (int(point[0]), int(point[1]))
+        """
+        cv2.pointPolygonTest(): A function from the OpenCV library that checks whether a point is inside a polygon.
+        self.pixel_vertices: The vertices of the polygon.
+        p: The point to be tested.
+        False: A flag indicating that the function should only test if the point is inside the polygon and not calculate the distance to the edge.
+        """
+        is_inside = cv2.pointPolygonTest(self.pixel_vertices, p, False) >= 0
+        # if the point is outside the polygon then execution ends
+        if not is_inside:
+            return None
+
+        """
+        point.reshape(-1, 1, 2): Reshapes the point to a specific format required by the cv2.perspectiveTransform() function.
+        -1: Infers the dimension size from the other given dimensions.
+        1: The number of rows.
+        2: The number of columns (for the x and y coordinates).
+        AND
+        astype(np.float32) Converts the point data type to float32.
+        """
+        reshaped_point = point.reshape(-1, 1, 2).astype(np.float32)
+        transform_point = cv2.perspectiveTransform(
+            reshaped_point, self.persepctive_transformer)  # the transformed point
+        return transform_point.reshape(-1, 2)
+
     def add_transformed_position_to_tracks(self, tracks):
         # Iterating over each object and its tracks in the dictionary
         for object, object_tracks in tracks.items():
