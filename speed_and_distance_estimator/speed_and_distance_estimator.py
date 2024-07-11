d7ce81d14129d3e1ac286772f234ad7708ab13b2
Aditya Mishra <mishraaaditya29@gmail.com>
2024-07-11 10:27:49 +0530

    speed and distance covered added

--- speed_and_distance_estimator/speed_and_distance_estimator.py
+++ speed_and_distance_estimator/speed_and_distance_estimator.py
@@ -1,3 +1,7 @@
+from utils import measure_distance, get_foot_position
+import cv2
+import sys
+sys.path.append('../')
 
 
 class SpeedAndDistance_Estimator():
@@ -5,3 +9,30 @@ class SpeedAndDistance_Estimator():
         # Calculating speed of user every 5 frame
         self.frame_window = 5
         self.frame_rate = 24  # frame rate 24 frame per second
+
+    def add_speed_and_distance_to_tracks(self, tracks):
+
+        for object, object_tracks in tracks.items():
+            if object == "ball" or object == "referees":
+                continue
+            number_of_frames = len(object_tracks)
+            for frame_num in range(0, number_of_frames, self.frame_window):
+                last_frame = min(frame_num+self.frame_window,
+                                 number_of_frames-1)
+
+                # if the track is in first frame but not in last frame then we continue
+                for track_id, _ in object_tracks[frame_num].items():
+                    if track_id not in object_tracks[last_frame]:
+                        continue
+
+                    start_position = object_tracks[frame_num][track_id]['position_transformed']
+                    end_position = object_tracks[last_frame][track_id]['position_transformed']
+
+                    if start_position is None or end_position is None:
+                        continue
+
+                    distance_covered = measure_distance(
+                        start_position, end_position)
+                    time_elapsed = (last_frame-frame_num)/self.frame_rate
+                    speed_meteres_per_second = distance_covered/time_elapsed
+                    speed_km_per_hour = speed_meteres_per_second*3.6
